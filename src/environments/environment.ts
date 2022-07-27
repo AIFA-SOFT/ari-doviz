549dfd6cc05eb2cc6b0f89a9f17f6c1a74558d01
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2022-07-27 19:45:15 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,7 @@
 export const environment = {
   production: false,
   // serverUrl:'https://dise,ase.sh/v3',
-  firebase: {
-    apiKey: "AIzaSyCksBRlgpNuGE6_-sirUq7dAcsYJWbaXtw",
-    authDomain: "aridoviz.firebaseapp.com",
-    databaseURL: "https://aridoviz-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "aridoviz",
-    storageBucket: "aridoviz.appspot.com",
-    messagingSenderId: "269636041966",
-    appId: "1:269636041966:web:66d8db127fcf75fb4cc7ff",
-    measurementId: "G-TC2VTGCXFN"
-  }
+  firebase: {}
 };
 
 /*
