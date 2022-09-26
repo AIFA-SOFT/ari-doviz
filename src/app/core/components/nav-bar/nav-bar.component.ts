66381048f4af8258819cc50d83c2b5d16e9a8b56
oguzhan18 <oguzhancart1@gmail.com>
2022-09-26 15:30:46 +0300

    We fulfill customer requests

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -12,16 +12,6 @@ import { ThemeService } from '../../services/theme.service';
 export class NavBarComponent implements OnInit {
   date: number = Date.now();
   lightTheme: boolean = false;
-  connection = webSocket(servers.real);
-  socketitems: SocketData[] = [];
-  sarrafiyesocketitems: SarrafiyeSocketData[] = [];
-  pariteList: SocketData[] = [];
-  footerList: SocketData[] = [];
-  footerListReplace: SocketData[] = [];
-  miktar:any;
-  sonuc:any;
-  secilen:any;
-  myDropDown : string="";
 
   constructor(private themeService: ThemeService) {}
   isShown: boolean = false;
@@ -29,84 +19,88 @@ export class NavBarComponent implements OnInit {
     this.lightTheme = !this.lightTheme;
     this.themeService.toggleTheme();
   }
-  ngOnInit() {
-    this.CurrencyConverter();
-  }
+  // ngOnInit() {
+  //   this.CurrencyConverter();
+  // }
 
 
-  CurrencyConverter() {
-  this.connection.subscribe((data: SocketData[]| any) => {
-    if (this.footerList) {
-      this.footerList = [];
-    }
+//   CurrencyConverter() {
+//   this.connection.subscribe((data: SocketData[]| any) => {
+//     if (this.footerList) {
+//       this.footerList = [];
+//     }
 
-    data.forEach((item: SocketData) => {
-      if (item.Category === CATEGORYTYPE.DOVIZ || item.Category === CATEGORYTYPE.PARITE) {
-        this.footerList.push(item);
-      }
-      if (  item.Category === CATEGORYTYPE.PARITE) {
-        this.pariteList.push(item);
-        // console.log(item.Ask * this.miktar);
-      }
-    });
-    if (this.footerListReplace.length !== 0) {
-      if (JSON.stringify(this.footerListReplace) === JSON.stringify(this.footerList)) {
-      } else {
-        this.footerList.forEach((data, index) => {
+//     data.forEach((item: SocketData) => {
+//       if (item.Category === CATEGORYTYPE.DOVIZ || item.Category === CATEGORYTYPE.PARITE) {
+//         this.footerList.push(item);
+//       }
+//       if (  item.Category === CATEGORYTYPE.PARITE) {
+//         this.pariteList.push(item);
+//         // console.log(item.Ask * this.miktar);
+//       }
+//     });
+//     if (this.footerListReplace.length !== 0) {
+//       if (JSON.stringify(this.footerListReplace) === JSON.stringify(this.footerList)) {
+//       } else {
+//         this.footerList.forEach((data, index) => {
+
+//           if (data.Ask !== this.footerListReplace[index].Ask) {
+//             this.percentChange(data, this.footerListReplace[index], index);
+//           } else {
+//             data.askPercentChange = 0.00;
+//             this.footerListReplace[index].askPercentChange = data.askPercentChange;
+
+//           }
+//         });
+//       }
+//     } else {
+//       this.footerListReplace = this.footerList;
+//     }
+//   });
+
+// }
 
 
-        // this.sonuc = this.secilen * this.miktar ;
+isCollapsed = false;
 
-          if (data.Ask !== this.footerListReplace[index].Ask) {
-            this.percentChange(data, this.footerListReplace[index], index);
-          } else {
-            data.askPercentChange = 0.00;
-            this.footerListReplace[index].askPercentChange = data.askPercentChange;
-            // console.log(data.Ask * this.miktar);
-
-          }
-        });
-      }
-    } else {
-      this.footerListReplace = this.footerList;
-    }
-  });
-
+toggleNavbar() {
+  this.isCollapsed = !this.isCollapsed;
 }
 
 
-
-
-
-onChangeofOptions(newGov: any) {
-  //  console.log(newGov);
-    newGov = newGov;
-    this.sonuc =  newGov * this.miktar ;
-    console.log(this.sonuc);
+ngOnInit(): void {
 }
 
 
+// onChangeofOptions(newGov: any) {
+//   //  console.log(newGov);
+//     newGov = newGov;
+//     this.sonuc =  newGov * this.miktar ;
+//     console.log(this.sonuc);
+// }
 
-trackByPrice(index: number, code:any) {
-  return code.Ask;
-}
 
-percentChange(newData:any, oldData:any, index:any) {
-  if (newData.Ask != oldData.Ask) {
-    let oldAskPrice = +oldData.Ask;
-    let newAskPrice = +newData.Ask;
-    let askPriceDifference = (1 - (oldAskPrice / newAskPrice)) * 100;
-    newData.askPercentChange = +askPriceDifference.toFixed(2);
-    newData.Time = Date.now();
-    if (askPriceDifference < 0) {
-      const code = newData.Code;
-      const element = document.getElementById(code);
-    } else if (askPriceDifference > 0) {
-      const code = newData.Code;
-      const element = document.getElementById(code);
-    }
-  }
-  }
+
+// trackByPrice(index: number, code:any) {
+//   return code.Ask;
+// }
+
+// percentChange(newData:any, oldData:any, index:any) {
+//   if (newData.Ask != oldData.Ask) {
+//     let oldAskPrice = +oldData.Ask;
+//     let newAskPrice = +newData.Ask;
+//     let askPriceDifference = (1 - (oldAskPrice / newAskPrice)) * 100;
+//     newData.askPercentChange = +askPriceDifference.toFixed(2);
+//     newData.Time = Date.now();
+//     if (askPriceDifference < 0) {
+//       const code = newData.Code;
+//       const element = document.getElementById(code);
+//     } else if (askPriceDifference > 0) {
+//       const code = newData.Code;
+//       const element = document.getElementById(code);
+//     }
+//   }
+//   }
 
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,11 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { PricefooterComponent } from "../shared/components/pricefooter/pricefooter.component";
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { SocialNavComponent } from './components/social-nav/social-nav.component';
-import { CurrencyCalculatorComponent } from './components/currency-calculator/currency-calculator.component';
-import { FormsModule } from "@angular/forms";
-import { TestComponent } from "../home/components/test/test.component";
-import { FooterComponent } from './components/footer/footer.component';
-
-// const components = [
-
-
-// ];
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule,RouterModule,FormsModule],
-  exports: [ ]
+  imports: [CommonModule, RouterModule, FormsModule],
+  exports: [],
 })
-export class CoreModule { }
+export class CoreModule {}
